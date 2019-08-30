4ee61c4d3916a60a8fbd42e4a37d45d8691465f3
Mariano Brayotta <mbrayotta@sistran.com.ar>
2019-08-30 13:34:01 -0300

    Updates

---
+++ precache-manifest.970d42652736df3cb8f964b5f0461393.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87e6347512b9bf2e440574b127305389",
+    "url": "/BasicCardsWithSearch/index.html"
+  },
+  {
+    "revision": "b3b589033385ccddbffd",
+    "url": "/BasicCardsWithSearch/static/css/main.b625955b.chunk.css"
+  },
+  {
+    "revision": "2ab373573a2da60aeefa",
+    "url": "/BasicCardsWithSearch/static/js/2.b3f2c5c4.chunk.js"
+  },
+  {
+    "revision": "b3b589033385ccddbffd",
+    "url": "/BasicCardsWithSearch/static/js/main.dc7a3bcc.chunk.js"
+  },
+  {
+    "revision": "de9e1f3e7df7b6409c4e",
+    "url": "/BasicCardsWithSearch/static/js/runtime~main.f4911cec.js"
+  }
+]);
